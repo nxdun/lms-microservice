2849c023fca52b1c3060940464cc0abe3a704c86
Vish0111 <it21315282@my.sliit.lk>
2024-05-06 22:30:22 +0530

    Staff Login & Register Frontend Done .✅

---
+++ frontend/src/components/auth/staffPortal/signup.jsx
@@ -0,0 +1,219 @@
+import { Formik } from "formik";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Card, Checkbox, Grid, TextField, MenuItem, useTheme, Box, styled } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import * as Yup from "yup";
+
+// import useAuth from "app/hooks/useAuth";
+import { Paragraph } from "./Typography";
+
+// STYLED COMPONENTS
+const FlexBox = styled(Box)(() => ({
+  display: "flex",
+  alignItems: "center"
+}));
+
+const JustifyBox = styled(FlexBox)(() => ({
+  justifyContent: "center"
+}));
+
+const ContentBox = styled(JustifyBox)(() => ({
+  height: "100%",
+  padding: "32px",
+  background: "rgba(0, 0, 0, 0.01)"
+}));
+
+const JWTRegister = styled(JustifyBox)(() => ({
+  background: "#1A2038",
+  minHeight: "100vh !important",
+  "& .card": {
+    maxWidth: 800,
+    minHeight: 400,
+    margin: "1rem",
+    display: "flex",
+    borderRadius: 12,
+    alignItems: "center"
+  }
+}));
+
+// Initial login credentials
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  role: "",
+  remember: true
+};
+
+// Form field validation schema
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required("First name is required!"),
+  lastName: Yup.string().required("Last name is required!"),
+  email: Yup.string().email("Invalid Email address").required("Email is required!"),
+  password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required!"),
+  role: Yup.string().required("Role is required!")
+});
+
+
+export default function JwtRegister() {
+  const theme = useTheme();
+  //const { register } = useAuth();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const handleFormSubmit = (values) => {
+    setLoading(true);
+
+    try {
+      register(values.email, values.username, values.password);
+      navigate("/");
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <JWTRegister>
+      <Card className="card">
+        <Grid container>
+          <Grid item sm={6} xs={12}>
+            <ContentBox>
+              <img
+                width="100%"
+                alt="Register"
+                src="src/assets/images/Signup.gif"
+              />
+            </ContentBox>
+          </Grid>
+
+          <Grid item sm={6} xs={12}>
+            <Box p={4} height="100%">
+              <Formik
+                onSubmit={handleFormSubmit}
+                initialValues={initialValues}
+                validationSchema={validationSchema}>
+                {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+                  <form onSubmit={handleSubmit}>
+
+                    <TextField
+                      fullWidth
+                      size="small"
+                      type="text"
+                      name="firstName"
+                      label="First Name"
+                      variant="outlined"
+                      onBlur={handleBlur}
+                      value={values.firstName}
+                      onChange={handleChange}
+                      helperText={touched.firstName && errors.firstName}
+                      error={Boolean(errors.firstName && touched.firstName)}
+                      sx={{ mb: 3 }}
+                    />
+
+                    <TextField
+                      fullWidth
+                      size="small"
+                      type="text"
+                      name="lastName"
+                      label="Last Name"
+                      variant="outlined"
+                      onBlur={handleBlur}
+                      value={values.lastName}
+                      onChange={handleChange}
+                      helperText={touched.lastName && errors.lastName}
+                      error={Boolean(errors.lastName && touched.lastName)}
+                      sx={{ mb: 3 }}
+                    />
+
+                    <TextField
+                      fullWidth
+                      size="small"
+                      type="email"
+                      name="email"
+                      label="Email"
+                      variant="outlined"
+                      onBlur={handleBlur}
+                      value={values.email}
+                      onChange={handleChange}
+                      helperText={touched.email && errors.email}
+                      error={Boolean(errors.email && touched.email)}
+                      sx={{ mb: 3 }}
+                    />
+                    <TextField
+                      fullWidth
+                      size="small"
+                      name="password"
+                      type="password"
+                      label="Password"
+                      variant="outlined"
+                      onBlur={handleBlur}
+                      value={values.password}
+                      onChange={handleChange}
+                      helperText={touched.password && errors.password}
+                      error={Boolean(errors.password && touched.password)}
+                      sx={{ mb: 2 }}
+                    />
+
+                    <TextField
+                      fullWidth
+                      size="small"
+                      select
+                      name="role"
+                      label="Role"
+                      variant="outlined"
+                      onBlur={handleBlur}
+                      value={values.role}
+                      onChange={handleChange}
+                      helperText={touched.role && errors.role}
+                      error={Boolean(errors.role && touched.role)}
+                      sx={{ mb: 3 }}
+                    >
+                      <MenuItem value="Admin">Admin</MenuItem>
+                      <MenuItem value="Instructor">Instructor</MenuItem>
+                    </TextField>
+
+                    <FlexBox gap={1} alignItems="center">
+                      <Checkbox
+                        size="small"
+                        name="remember"
+                        onChange={handleChange}
+                        checked={values.remember}
+                        sx={{ padding: 0 }}
+                      />
+
+                      <Paragraph fontSize={13}>
+                        I have read and agree to the terms of service.
+                      </Paragraph>
+                    </FlexBox>
+
+                    <LoadingButton
+                      type="submit"
+                      color="primary"
+                      loading={loading}
+                      variant="contained"
+                      sx={{ mb: 2, mt: 3 }}>
+                      Register
+                    </LoadingButton>
+
+                    <Paragraph>
+                      Already have an account?
+                      <NavLink
+                        to="/stafflogin"
+                        style={{ color: theme.palette.primary.main, marginLeft: 5 }}>
+                        Login
+                      </NavLink>
+                    </Paragraph>
+                  </form>
+                )}
+              </Formik>
+            </Box>
+          </Grid>
+        </Grid>
+      </Card>
+    </JWTRegister>
+  );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -36,6 +36,12 @@ import {
   ManageCourse
 } from "src/components/admindashboard/scenes";
 
+
+
+//Staff Portal
+import { StaffLogin, NotFound, StaffRegister, ForgetPWD } from "src/components/auth/staffPortal";
+
+
 const apiUrl = 'http://localhost:3001';//API URL for paymeent gateway
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -69,6 +75,14 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/register" element={<RegisterScreen />} />
         <Route path="/browse" element={<BrowseScreen />} />
         <Route path="/browse/view/:id" element={<CourseSPA />} />
+
+
+        {/*Staff Routes */}
+        <Route path="/stafflogin" element={<StaffLogin />} />
+        <Route path="/staffregister" element={<StaffRegister />} />
+        <Route path="/staffforgot" element={<ForgetPWD />} />
+        <Route path="/staff/*" element={<NotFound />} />
+
 
 
         {/*Payment Routes */}
