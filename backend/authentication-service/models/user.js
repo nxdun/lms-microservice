2a4fb590537c9a05ca6c62912d17abea882fc29e
it21813702 <it21813702@my.sliit.lk>
2024-05-06 19:14:42 +0530

    added enum property to roles for data integrity

--- backend/authentication-service/models/user.js
+++ backend/authentication-service/models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    role: { type: String, default: 'user' },
+    role: { type: String, enum: ['student', 'lecturer', 'admin' ], default: 'user' },
 });
 
 //token generation method
