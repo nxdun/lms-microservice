af9d673bd08fa6d83e16a6f20dfab67ee300f45e
it21813702 <it21813702@my.sliit.lk>
2024-05-06 08:21:06 +0530

    added comments

--- backend/authentication-service/routes/auth.js
+++ backend/authentication-service/routes/auth.js
@@ -5,21 +5,25 @@ const { logger } = require('./logger');
 
 //validate function
 const joi = require('joi');
-
 const bcrypt = require('bcryptjs');
 
 router.post("/", async (req, res) => {
     try{
+        //validate user input data
         const {error} = validate(req.body);
         if(error){
             logger.error('Validation error:', error);
             return res.status(400).send(`val error ${error.details[0].message} error is  ${error} body ${JSON.stringify(req.body)}`);
         }    
+
+        //find user with same email
         const user = await User.findOne({email: req.body.email});
         if(!user){
             logger.info('Invalid email or password:', req.body.email);
             return res.status(401).send({message: "Invalid email or password!"});
         }
+
+        //compare passwords
         const validPassword = await  bcrypt.compare(
             req.body.password, user.password
             );  
@@ -27,10 +31,14 @@ router.post("/", async (req, res) => {
             logger.info('Invalid email or password:', req.body.email);
             return res.status(401).send({message: "Invalid email or password!"});
         }
+
+        //generate authentication token
         const token = user.generateAuthToken();
         logger.info('Login successful for user:', req.body.email);
         res.status(200).send({ data: token,  message: "Login successful!" });
+
     }catch(error){
+        //error handling
         logger.error('Internal server error:', error);
         res.status(500).send({message: "Internal server error!"});
 
