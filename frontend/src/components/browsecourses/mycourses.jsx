320bdd426dde8ab994aa06997ae647f722bcb9a7
Nadu_Dev <inbox.nadun@gmail.com>
2024-05-08 10:02:35 +0530

    some minor changes

--- frontend/src/components/browsecourses/mycourses.jsx
+++ frontend/src/components/browsecourses/mycourses.jsx
@@ -1,94 +1,128 @@
-import { useEffect, useState } from 'react';
-import { Box, Card, Grid, Avatar, Typography, CardContent, Button } from "@mui/material";
-import { Book } from '@mui/icons-material';
-import axios from 'axios';
-import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Card,
+  Grid,
+  Avatar,
+  Typography,
+  CardContent,
+  Button,
+} from "@mui/material";
+import axios from "axios";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+import DynamicBackdrop from "../common/backdrop";
 const MyCourses = () => {
-    const [courseData, setCourseData] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [courseData, setCourseData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [Pc, setPc] = useState(40);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const coursesResponse = await axios.get("http://localhost:5000/browse");
-                const courses = coursesResponse.data;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const coursesResponse = await axios.get("http://localhost:5000/browse");
+        const courses = coursesResponse.data;
 
-                const coursesWithLecturers = await Promise.all(
-                    courses.map(async (course) => {
-                        const lecturerResponse = await axios.get(
-                            `http://localhost:5000/lecget/${course.lecturer_ID}`
-                        );
-                        const lecturerData = lecturerResponse.data;
-                        return { ...course, lecturer: lecturerData };
-                    })
-                );
+        const coursesWithLecturers = await Promise.all(
+          courses.map(async (course) => {
+            const lecturerResponse = await axios.get(
+              `http://localhost:5000/lecget/${course.lecturer_ID}`
+            );
+            const lecturerData = lecturerResponse.data;
+            return { ...course, lecturer: lecturerData };
+          })
+        );
 
-                setCourseData(coursesWithLecturers);
-                setLoading(false);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        fetchData();
-
-        // Disable horizontal scrolling on mount
-        document.body.style.overflowX = "hidden";
-
-        // Re-enable horizontal scrolling on unmount
-        return () => {
-            document.body.style.overflowX = "auto";
-        };
-    }, []);
-
-    const handleLearnNowClick = (courseId) => {
-        // Handle the Learn Now button click event
-        console.log(`Learn Now clicked for course ID: ${courseId}`);
+        setCourseData(coursesWithLecturers);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-            }}
-        >
-            <Grid container spacing={3} justifyContent="center">
-                {courseData.map((course) => (
-                    <Grid item key={course._id} xs={12} sm={6} md={4} lg={11}>
-                        <Card sx={{ height: "100%" }}>
-                            <CardContent>
-                                <Typography variant="h5" gutterBottom>
-                                    {course.course_title}
-                                </Typography>
-                                <Typography variant="body1" gutterBottom>
-                                    {course.course_description}
-                                </Typography>
-                                <div style={{ display: "flex", alignItems: "center", marginTop: "10px" }}>
-                                    <Avatar src={course.lecturer.ppic} alt={course.lecturer.name} />
-                                    <Typography variant="body2" style={{ marginLeft: "10px" }}>
-                                        {course.lecturer.name}
-                                    </Typography>
-                                </div>
-                            </CardContent>
-                            <Box sx={{ display: 'flex', justifyContent: 'flex-end', padding: '8px' }}>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    endIcon={<LocalLibraryIcon />}
-                                    onClick={() => handleLearnNowClick(course._id)}
-                                >
-                                    Learn Now
-                                </Button>
-                            </Box>
-                        </Card>
-                    </Grid>
-                ))}
+    fetchData();
+
+    // Disable horizontal scrolling on mount
+    document.body.style.overflowX = "hidden";
+
+    // Re-enable horizontal scrolling on unmount
+    return () => {
+      document.body.style.overflowX = "auto";
+    };
+  }, []);
+
+  const handleLearnNowClick = (courseId) => {
+    // Handle the Learn Now button click event
+    console.log(`Learn Now clicked for course ID: ${courseId}`);
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Grid container spacing={3} justifyContent="center">
+          {courseData.map((course) => (
+            <Grid item key={course._id} xs={12} sm={6} md={4} lg={11}>
+              <Card sx={{ height: "100%" }}>
+                <CardContent>
+                  <Typography variant="h5" gutterBottom>
+                    {course.course_title}
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    {course.course_description}
+                  </Typography>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      marginTop: "10px",
+                    }}
+                  >
+                    <Avatar
+                      src={course.lecturer.ppic}
+                      alt={course.lecturer.name}
+                    />
+                    <Typography variant="body2" style={{ marginLeft: "10px" }}>
+                      {course.lecturer.name}
+                    </Typography>
+                  </div>
+                </CardContent>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    padding: "10px",
+                  }}
+                >
+                  Progress:
+                  <Box sx={{ width: "100%" }}></Box>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    endIcon={<LocalLibraryIcon />}
+                    onClick={() => handleLearnNowClick(course._id)}
+                    sx={{
+                      margin: "0 1vh 1vh 0",
+                      width: "30vh",
+                      height: "10vh",
+                    }}
+                  >
+                    Learn Now
+                  </Button>
+                </Box>
+              </Card>
             </Grid>
-        </Box>
-    );
+          ))}
+        </Grid>
+      </Box>
+      <DynamicBackdrop open={loading} />
+    </>
+  );
 };
 
 export default MyCourses;
---
+++ frontend/src/components/common/linearProgress.jsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '100%', mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value,
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+LinearProgressWithLabel.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+
+export default function LinearWithValueLabel() {
+
+    const [Pc, setPc] = React.useState(0);
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+    }, 800);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <LinearProgressWithLabel value={Pc} Pc = {Pc} setPc= {setPc}/>
+    </Box>
+  );
+}
