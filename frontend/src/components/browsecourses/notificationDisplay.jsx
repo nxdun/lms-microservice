6ccd6b195bab69cfd55103ffdebf5be4792f3fb2
it21813702 <it21813702@my.sliit.lk>
2024-05-09 21:38:12 +0530

    added descriptions

--- frontend/src/components/browsecourses/notificationDisplay.jsx
+++ frontend/src/components/browsecourses/notificationDisplay.jsx
@@ -18,18 +18,24 @@ import axios from "axios";
 
 const types = ["success", "info", "warning", "error"];
 
+
 export default function DescriptionAlerts() {
+  //state variables for notifications and unread count
   const [notifications, setNotifications] = useState([]);
   const [unreadCount, setUnreadCount] = useState(0);
+  
+  //state variables for controlling notification center(popper) visibility and filter for unread only
   const [showUnreadOnly, setShowUnreadOnly] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
 
+  //function to toggle popper visibility
   const toggleNotificationCenter = (event) => {
     setAnchorEl(event.currentTarget);
     setIsOpen(!isOpen);
   };
 
+  //function to toggle filter for unread only
   const toggleFilter = () => {
     setShowUnreadOnly(!showUnreadOnly);
   };
@@ -38,13 +44,16 @@ export default function DescriptionAlerts() {
     // Fetch notifications from the server
     const fetchNotifications = async () => {
       try {
+
         //remove 1234 and replace with the user id taken from the user's session: N
         const response = await axios.get("http://localhost:5000/notify/1234");
         const fetchedNotifications = response.data;
         setNotifications(fetchedNotifications);
+
         // Calculate unread count
         const newUnreadCount = fetchedNotifications.filter(notification => !notification.read).length;
         setUnreadCount(newUnreadCount);
+
       } catch (error) {
         console.error("Error fetching notifications:", error);
       }
@@ -53,6 +62,7 @@ export default function DescriptionAlerts() {
     fetchNotifications(); // Fetch notifications when the component mounts
   }, []); // Empty dependency array ensures that this effect runs only once, similar to componentDidMount
 
+  //function to mark notifications as read
   const markNotificationAsRead = (id) => {
     const updatedNotifications = notifications.map(notification => {
       if (notification._id === id) {
