f475c99b2aa496f212b75d72c11e837a7eecf993
Vish0111 <it21315282@my.sliit.lk>
2024-05-09 16:39:21 +0530

    Success and Cancel Pages Updated

--- frontend/src/components/payment/Cancel.jsx
+++ frontend/src/components/payment/Cancel.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function CancelPage() {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate('/');
+  };
+
   return (
-    <div>
-      <h1>Payment Cancelled</h1>
-      <p>Your payment was cancelled. If you have any questions, please contact support.</p>
-    </div>
+    <Box m={3} display="flex" flexDirection="column" alignItems="center">
+      <Box p={3} boxShadow={3} borderRadius={8} bgcolor="background.paper" textAlign="center">
+        <img src="src/assets/images/Cancel.gif" alt="Cancelled" style={{ width: '320px', height: '320px' }} />
+        <Typography variant="h4" gutterBottom>
+          Payment Cancelled
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Your payment was cancelled. If you have any questions, please contact support.
+        </Typography>
+        <Button variant="contained" color="primary" onClick={handleGoBack}>
+          Go Back
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
