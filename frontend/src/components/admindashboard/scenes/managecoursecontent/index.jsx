08a987a44f0b66b5a86eea2aad7a446e0cf00331
Vish0111 <it21315282@my.sliit.lk>
2024-05-11 16:46:04 +0530

    course content delete update added

--- frontend/src/components/admindashboard/scenes/managecoursecontent/index.jsx
+++ frontend/src/components/admindashboard/scenes/managecoursecontent/index.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Card, CardContent, Typography, Dialog, DialogTitle, DialogContent, Button } from '@mui/material';
+import { Grid, Card, CardContent, Typography, Dialog, DialogTitle, DialogContent, Button, CircularProgress } from '@mui/material';
 
 function CoursesContent() {
   const [courses, setCourses] = useState([]);
   const [courseTitles, setCourseTitles] = useState({});
   const [selectedCourse, setSelectedCourse] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
-  const [showMoreContent, setShowMoreContent] = useState(false);
-  const [selectedCourseId, setSelectedCourseId] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Fetch course content
@@ -24,11 +23,12 @@ function CoursesContent() {
         const courseIds = data.map(course => course.courseId);
         fetchCourseTitles(courseIds);
       })
-      .catch(error => console.error('Error fetching courses:', error));
+      .catch(error => console.error('Error fetching courses:', error))
+      .finally(() => setLoading(false));
   }, []);
 
-  const fetchCourseTitles = (courseIds) => {
-    Promise.all(courseIds.map(courseId =>
+  const fetchCourseTitles = (courseIdList) => {
+    Promise.all(courseIdList.map(courseId =>
       fetch(`http://localhost:3002/api/v1/courses/${courseId}`)
         .then(response => response.json())
         .then(data => {
@@ -37,100 +37,90 @@ function CoursesContent() {
             [courseId]: data.course_title // Adjusted to match your database field
           }));
         })
+        .catch(error => console.error(`Error fetching course title for ${courseId}:`, error))
     ));
-  };;
+  };
 
-  // Open dialog with details of selected course
   const handleCourseClick = (course) => {
     setSelectedCourse(course);
     setOpenDialog(true);
   };
 
-  // Toggle show more content
-  const toggleShowMoreContent = (courseId) => {
-    setShowMoreContent(!showMoreContent);
-    setSelectedCourseId(courseId);
+  const handleDeleteCourse = async (id) => {
+    try {
+      await fetch(`http://localhost:3002/api/v1/content/${id}`, {
+        method: 'DELETE',
+      });
+      setCourses(courses.filter(course => course._id !== id));
+      // Optionally, you can also close the dialog if the deleted course was currently selected
+      if (selectedCourse && selectedCourse._id === id) {
+        setOpenDialog(false);
+      }
+    } catch (error) {
+      console.error('Error deleting course:', error);
+    }
+  };
+
+  const handleUpdateCourse = async (id) => {
+    // Implement your update logic here
+    console.log('Update course with ID:', id);
   };
 
   return (
     <div>
       <h1>Courses Contents</h1>
-      <Grid container spacing={2}>
-        {courses.map((course, index) => (
-          <Grid key={course._id} item xs={12} sm={6} md={6} lg={6} xl={6}>
-            <Card onClick={() => handleCourseClick(course)} style={{ cursor: 'pointer' }}>
-              <CardContent>
-              <Typography variant="h6" component="div">Course: {courseTitles[course.courseId]}</Typography>
-                {!showMoreContent && selectedCourseId !== course._id &&
-                  <Button onClick={() => toggleShowMoreContent(course._id)}>Show More</Button>
-                }
-                {showMoreContent && selectedCourseId === course._id &&
-                  <>
-                    <Typography variant="subtitle1" component="div">Video URLs:</Typography>
-                    <ul>
-                      {course.videoUrls.map((url, index) => (
-                        <li key={index}>{url}</li>
-                      ))}
-                    </ul>
-                    <Typography variant="subtitle1" component="div">Chapters:</Typography>
-                    <ul>
-                      {course.chapters.map((chapter, index) => (
-                        <li key={index}>{chapter}</li>
-                      ))}
-                    </ul>
-                    <Typography variant="subtitle1" component="div">Quizzes:</Typography>
-                    <ul>
-                      {course.quizzes.map((quiz, index) => (
-                        <li key={index}>
-                          <Typography variant="body1">{quiz.question}</Typography>
-                          <ul>
-                            {quiz.options.map((option, optionIndex) => (
-                              <li key={optionIndex}>{option}</li>
-                            ))}
-                          </ul>
-                          <Typography variant="body2">Correct Answer Index: {quiz.correctAnswerIndex}</Typography>
-                        </li>
-                      ))}
-                    </ul>
-                    <Button onClick={() => toggleShowMoreContent(course._id)}>Show Less</Button>
-                  </>
-                }
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-      {/* Dialog to display more details of selected course */}
+      {loading ? (
+        <CircularProgress /> // Show loading indicator
+      ) : (
+        <Grid container spacing={2}>
+          {courses.map((course, index) => (
+            <Grid key={course._id || index} item xs={12} sm={6} md={6} lg={6} xl={6}>
+              <Card style={{ marginBottom: '20px' }}>
+                <CardContent>
+                  <Typography variant="h6" component="div">Course: {courseTitles[course.courseId]}</Typography>
+                  <Button onClick={() => handleCourseClick(course)} style={{ color: 'white' }} variant="contained" >View Details</Button>
+                  <Button onClick={() => handleUpdateCourse(course._id)} style={{ marginLeft: '10px' }} variant="contained" color="primary">Update</Button>
+                  <Button onClick={() => handleDeleteCourse(course._id)} style={{ marginLeft: '10px' }} variant="contained" color="error">Delete</Button>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
       <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
-        <DialogTitle>{selectedCourse && courseTitles[selectedCourse.courseId]}</DialogTitle>
-        <DialogContent>
-          <Typography variant="subtitle1" component="div">Video URLs:</Typography>
-          <ul>
-            {selectedCourse && selectedCourse.videoUrls.map((url, index) => (
-              <li key={index}>{url}</li>
-            ))}
-          </ul>
-          <Typography variant="subtitle1" component="div">Chapters:</Typography>
-          <ul>
-            {selectedCourse && selectedCourse.chapters.map((chapter, index) => (
-              <li key={index}>{chapter}</li>
-            ))}
-          </ul>
-          <Typography variant="subtitle1" component="div">Quizzes:</Typography>
-          <ul>
-            {selectedCourse && selectedCourse.quizzes.map((quiz, index) => (
-              <li key={index}>
-                <Typography variant="body1">{quiz.question}</Typography>
-                <ul>
-                  {quiz.options.map((option, optionIndex) => (
-                    <li key={optionIndex}>{option}</li>
-                  ))}
-                </ul>
-                <Typography variant="body2">Correct Answer Index: {quiz.correctAnswerIndex}</Typography>
-              </li>
-            ))}
-          </ul>
-        </DialogContent>
+        {selectedCourse && (
+          <>
+            <DialogTitle>{courseTitles[selectedCourse.courseId]}</DialogTitle>
+            <DialogContent>
+              <Typography variant="subtitle1" component="div">Video URLs:</Typography>
+              <ul>
+                {selectedCourse.videoUrls.map((url, index) => (
+                  <li key={index}>{url}</li>
+                ))}
+              </ul>
+              <Typography variant="subtitle1" component="div">Chapters:</Typography>
+              <ul>
+                {selectedCourse.chapters.map((chapter, index) => (
+                  <li key={index}>{chapter}</li>
+                ))}
+              </ul>
+              <Typography variant="subtitle1" component="div">Quizzes:</Typography>
+              <ul>
+                {selectedCourse.quizzes.map((quiz, index) => (
+                  <li key={index}>
+                    <Typography variant="body1">{quiz.question}</Typography>
+                    <ul>
+                      {quiz.options.map((option, optionIndex) => (
+                        <li key={optionIndex}>{option}</li>
+                      ))}
+                    </ul>
+                    <Typography variant="body2">Correct Answer Index: {quiz.correctAnswerIndex}</Typography>
+                  </li>
+                ))}
+              </ul>
+            </DialogContent>
+          </>
+        )}
       </Dialog>
     </div>
   );
